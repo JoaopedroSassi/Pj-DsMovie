9183a570869a86e7e46b4a76b6f171012a6c6e61
Sassi <joaopedro10813@gmail.com>
2022-01-12 22:51:07 -0300

    Navbar

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,8 +5,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+   <React.StrictMode>
+      <App />
+   </React.StrictMode>,
+   document.getElementById('root')
 );
